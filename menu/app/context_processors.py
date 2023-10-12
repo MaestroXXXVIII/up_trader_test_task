60d72b03bde24b71e08354dc07c59e9c27872009
MaestroXXXVIII <urmanov.2000@mail.ru>
2023-10-12 17:26:50 +0800

    Removing unnecessary code

--- menu/app/context_processors.py
+++
@@ -1,3 +0,0 @@
-def app_context(request):
-    current_path_name = f'{request.scheme}://{request.get_host()}'
-    return locals()
--- menu/app/views.py
+++ menu/app/views.py
@@ -5,6 +5,4 @@ from .models import MenuItem
 
 
 def menu_view(request, title):
-    current_path_name = f'{request.scheme}://{request.get_host()}'
-    return render(request, 'app/index.html', {'title': title,
-                                              'path': current_path_name})
+    return render(request, 'app/index.html', {'title': title, })
